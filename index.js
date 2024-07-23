bc7ddf73581c4f4b18a795895eeaeff515092582
brenki1 <pagioliosvaldo@gmail.com>
2024-07-23 15:43:21 -0300

    adicionado configuraçoes do dotenv

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+import dotenv from "dotenv"
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
+dotenv.config();
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 async function run() {
   // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
   const prompt = "Write like a virtual assistant, telling how you can help and how you can improve this business workflow."
 
