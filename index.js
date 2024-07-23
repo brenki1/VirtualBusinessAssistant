81f3becdbc8addae0ebdc1a5a27f7bfe9cb76309
Gustavo <gustavo24104@gmail.com>
2024-07-23 15:14:26 -0300

    adicionado código de exemplo da google arrumado packages.json

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 // Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const genAI = new GoogleGenerativeAI('');
 
 async function run() {
   // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
-  const prompt = "Write a story about a magic backpack."
+  const prompt = "Write like a virtual assistant, telling how you can help and how you can improve this business workflow."
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
