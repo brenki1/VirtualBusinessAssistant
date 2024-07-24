ae2ebaa173cb28ae81be2c89c5c71bdcf24f37f9
brenki1 <pagioliosvaldo@gmail.com>
2024-07-24 19:19:38 -0300

    agora é possivel conversar com a IA

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 import dotenv from "dotenv"
 import { GoogleGenerativeAI } from "@google/generative-ai"
+import readline from "readline"
 
 dotenv.config();
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+async function conversar() {
+
+    const model = genAI.getGenerativeModel({model: "gemini-1.5-pro" });
+
+    const chat = model.startChat({
+        history: [],
+        generationConfig: {
+            maxOutputTokens:500,
+        },
+    });
+
+    async function askAndRespond() {
+        rl.question("You: ", async (msg) => {
+            if(msg.toLowerCase === "exit") {
+                rl.close();
+            } else {
+                const result = await chat.sendMessage(msg);
+                const response = await result.response;
+                const text = await response.text();
+                console.log("AI: ", text);
+                askAndRespond();
+            }
+        });
+    }
+
+    askAndRespond();
+}
+
+conversar();
 
 async function teste() {
 
@@ -14,9 +50,6 @@ async function teste() {
     const response = await result.response;
     const text = response.text();
     console.log(text);
-
-
-
 }
 
 async function teste2() {
@@ -30,5 +63,5 @@ async function teste2() {
     console.log(text);
 }
 
-teste();
-teste2();
+//teste();
+//teste2();
