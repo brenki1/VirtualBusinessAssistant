c39838901ada78aac6a3b7457efe5cdb5d122595
brenki1 <pagioliosvaldo@gmail.com>
2024-07-24 16:37:51 -0300

    readicionado index basico, .gitignore restaurado

---
+++ index.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv"
+import { GoogleGenerativeAI } from "@google/generative-ai"
+
+dotenv.config();
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+async function teste() {
+
+    const model = genAI.getGenerativeModel({model: "gemini-1.5-pro" });
+
+    const prompt = "Write a brief text about programming languages, and point out which ones are the best for beginners and which ones are the most advanced."
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+
+}
+
+teste();
