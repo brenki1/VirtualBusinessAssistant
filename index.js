4a2622697864616e92bcc86165c0246cb94c9b1d
brenki1 <pagioliosvaldo@gmail.com>
2024-07-24 16:46:31 -0300

    adicionado mais um exemplo

--- index.js
+++ index.js
@@ -10,12 +10,25 @@ async function teste() {
     const model = genAI.getGenerativeModel({model: "gemini-1.5-pro" });
 
     const prompt = "Write a brief text about programming languages, and point out which ones are the best for beginners and which ones are the most advanced."
-
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
     console.log(text);
 
+
+
+}
+
+async function teste2() {
+
+    const model = genAI.getGenerativeModel({model: "gemini-1.5-pro" });
+
+    const prompt = "Write a text about the best beginner linux distros."
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
 }
 
 teste();
+teste2();
