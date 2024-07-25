85628ca05870cd4032d1b6e5302196b2e2158d49
brenki1 <pagioliosvaldo@gmail.com>
2024-07-25 20:12:11 -0300

    pequenas alteraçoes

--- assistente-virtual-negocios/src/App.js
+++ assistente-virtual-negocios/src/App.js
@@ -1,6 +1,30 @@
+import { useState } from "react";
+
 const App = () => {
+    const [error, setError] = useState("Algo deu errado!")
   return (
-    
+    <div className="app">
+      <section className="search-section">
+        <p>Boa tarde, jovem!
+          <button className="surprise">Me surpreenda</button>
+        </p>
+          <div className="input-container">
+            <input
+                value={""}
+                placeholder="Osvaldo"
+                onChange={""}
+            />
+            {!error && <button>Me pergunte</button>}
+            {error &&<button>Limpar</button>}
+          </div>
+          {error && <p>{error}</p>}
+          <div className="search-result">
+              <div key={""}>
+                <p className="answer"></p>
+              </div>
+          </div>
+      </section>
+    </div>
   )
 }
 
