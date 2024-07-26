0efcc5a749ed697c32731a46b076e48fdc4b897c
Gustavo <gustavo24104@gmail.com>
2024-07-25 21:43:56 -0300

    adicionado um historico para os chats

--- assistente-virtual-negocios/src/index.js
+++ assistente-virtual-negocios/src/index.js
@@ -1,8 +1,8 @@
 import dotenv from "dotenv"
 import { GoogleGenerativeAI } from "@google/generative-ai"
 import readline from "readline"
-
 dotenv.config();
+import fs from "fs";
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
@@ -13,22 +13,38 @@ const rl = readline.createInterface({
 
 async function conversar() {
 
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
-
-    const chat = model.startChat({
-        history: [],
-        generationConfig: {
-            maxOutputTokens: 5000,
-        },
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro",
+        systemInstruction : "Answer in the same language that the user is typing." +
+        "Introduce yourself and offer to help" +  
+        "You are an assistant and finance expert helping a small business grow." 
+     + "Write in a positive, assertive and upbeat tone." +
+     "Ignore everything that's not related to business"
     });
 
+    const chat = model.startChat({
+        history: fs.open("chatLog.txt", "r", function (err, file) {
+            if(err) throw err;
+        }),
+        generationConfig: {
+            maxOutputTokens: 500,
+        },
+    });
     async function askAndRespond() {
         rl.question("You: ", async (msg) => {
             if (msg.toLowerCase() === "exit") {
-                console.log("Obrigado pela conversa!");
+                var historico = await chat.getHistory();
+                //console.log("Obrigado pela conversa! Aqui esta nosso historico de conversa: \n", historico);
+                fs.writeFile(
+                    "chatLog.txt",
+                    JSON.stringify(historico),
+                    function (err){
+                        if(err) console.error("algo deu errado!");
+                    }
+                );
                 rl.close();
             } else {
                 const result = await chat.sendMessage(msg);
+                let historico = await chat.getHistory();
                 const response = await result.response;
                 const text = await response.text();
                 console.log("AI: ", text);
