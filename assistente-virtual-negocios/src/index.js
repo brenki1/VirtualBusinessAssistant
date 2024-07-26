57bf64618f94f8fe7a5d7f54cdc33cdd89fcd089
Marcos Paulo Gomes <128519024+Marcospgp26@users.noreply.github.com>
2024-07-26 01:41:04 -0300

    Retirado algumas funções desnecessárias

--- assistente-virtual-negocios/src/index.js
+++ assistente-virtual-negocios/src/index.js
@@ -3,7 +3,6 @@ import { GoogleGenerativeAI } from "@google/generative-ai"
 import readline from "readline"
 dotenv.config();
 import fs from "fs";
-import { error } from "console";
 import oldLog from "./chatLog.json" with { type: "json" };
 
 
@@ -18,14 +17,15 @@ const rl = readline.createInterface({
 
 async function conversar() {
 
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro",
-        systemInstruction : "Answer in the same language that the user is typing." +
-        "Introduce yourself and offer to help" +  
-        "You are an assistant and finance expert helping a small business grow." 
-     + "Write in a positive, assertive and upbeat tone." +
-     "Ignore everything that's not related to business"
+    const model = genAI.getGenerativeModel({
+        model: "gemini-1.5-pro",
+        systemInstruction: "Answer in the same language that the user is typing." +
+            "Introduce yourself and offer to help" +
+            "You are an assistant and finance expert helping a small business grow."
+            + "Write in a positive, assertive and upbeat tone." +
+            "Ignore everything that's not related to business"
     });
-    
+
     //console.log(oldLog);
     const chat = model.startChat({
         history: oldLog,
@@ -41,14 +41,13 @@ async function conversar() {
                 fs.writeFile(
                     "chatLog.json",
                     JSON.stringify(historico),
-                    function (err){
-                        if(err) console.error("algo deu errado!");
+                    function (err) {
+                        if (err) console.error("algo deu errado!");
                     }
                 );
                 rl.close();
             } else {
                 const result = await chat.sendMessage(msg);
-                let historico = await chat.getHistory();
                 const response = await result.response;
                 const text = await response.text();
                 console.log("AI: ", text);
@@ -61,28 +60,3 @@ async function conversar() {
 }
 
 conversar();
-
-async function teste() {
-
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
-
-    const prompt = "Write a brief text about programming languages, and point out which ones are the best for beginners and which ones are the most advanced."
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
-    console.log(text);
-}
-
-async function teste2() {
-
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
-
-    const prompt = "Write a text about the best beginner linux distros."
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
-    console.log(text);
-}
-
-//teste();
-//teste2();
