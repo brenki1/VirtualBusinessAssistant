158f7aa914545c22d5e8927456bbbab42566b52f
brenki1 <pagioliosvaldo@gmail.com>
2024-07-31 20:01:41 -0300

    getter&setters + validação de CPF

--- assistente-virtual-negocios/src/dados/Pessoa.ts
+++ assistente-virtual-negocios/src/dados/Pessoa.ts
@@ -4,7 +4,7 @@ class Pessoa {
     private endereco : string;
     private nascimento : string;  //DD/MM/AA
     
-    constructor(CPF, nome, endereco, nascimento) {
+    constructor(CPF: string, nome: string, endereco: string, nascimento: string) {
         this.CPF = CPF;
         this.nome = nome;
         this.endereco = endereco;
@@ -12,12 +12,55 @@ class Pessoa {
     }
 
 
-    public getnome(){
+    public getCPF(): string {
+        return this.CPF;
+    }
+
+    public getNome(): string {
         return this.nome;
+    }
+
+    public getEndereco(): string {
+        return this.endereco;
+    }
+
+    public getNascimento(): string {
+        return this.nascimento;
+    }
+
+    public setCPF(CPF: string): void {
+        this.CPF = CPF;
+    }
+
+    public setNome(nome: string): void {
+        this.nome = nome;
+    }
+
+    public setEndereco(endereco: string): void {
+        this.endereco = endereco;
+    }
+
+    public setNascimento(nascimento: string): void {
+        this.nascimento = nascimento;
+    }
+
+    public validaCPF(cpf) {
+
+        if (typeof cpf !== 'string') return false
+        cpf = cpf.replace(/[^\d]+/g, '')
+        if (cpf.length !== 11 || !!cpf.match(/(\d)\1{10}/)) return false
+        cpf = cpf.split('').map(el => +el)
+        const rest = (count) => (cpf.slice(0, count-12)
+        .reduce( (soma, el, index) => (soma + el * (count-index)), 0 )*10) % 11 % 10
+        return rest(10) === cpf[9] && rest(11) === cpf[10]
+        
     }
 
 
 
-    // TODO: MÉTODOS GET&SET, que são meio diferentes em TS, ponto a ser discutido; MÉTODOS NO GERAL, EX: VALIDAÇÃO DE CPF
+    // TODO: MÉTODOS NO GERAL
+
+    //GET&SET ✅
+    //VALIDAÇAO DE CPF ✔️ (testar)
 
 }
